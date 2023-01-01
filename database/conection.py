23d6cf1a9533159c1025e090cbc79c25080ee81c
Alberto <albertolicea00@gmail.com>
2022-12-31 20:00:15 -0500

    repare el modulo de coneccion

--- database/conection.py
+++ database/conection.py
@@ -3,22 +3,16 @@ import sys, os, psycopg2
 
 
 load_dotenv()
-def Conection():
-    """
-        >>> con = Conection()
-        Database is Connected!
-    """
 
-    try:
-        conection = psycopg2.connect(
-            host = os.getenv('DB_HOST'),
-            port = os.getenv('DB_PORT'),
-            database = os.getenv('DB_DATABASE'),
-            user = os.getenv('DB_USERNAME'),
-            password = os.getenv('DB_PASSWORD')
-        )
-        print('Database is Connected!')
-        return conection
+try:
+    conection = psycopg2.connect(
+        host = os.getenv('DB_HOST'),
+        port = os.getenv('DB_PORT'),
+        database = os.getenv('DB_DATABASE'),
+        user = os.getenv('DB_USERNAME'),
+        password = os.getenv('DB_PASSWORD')
+    )
+    print('Database is Connected!')
 
-    except psycopg2.Error as e:
-        return("Error while connecting to database: ", e)
+except psycopg2.Error as e:
+    print("Error while connecting to database: ", e)
