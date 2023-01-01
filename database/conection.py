4cdf4f43deb395d76f82bd5ea513f4b81d834513
Alberto <albertolicea00@gmail.com>
2022-12-31 19:04:03 -0500

    ajuste la coneccion a la bbdd

--- database/conection.py
+++ database/conection.py
@@ -5,52 +5,18 @@ import psycopg2
 
 
 
-class Conection():
+load_dotenv()
+def conection():
+    try:
+        conection = psycopg2.connect(
+            host = os.getenv('DB_HOST'),
+            port = os.getenv('DB_PORT'),
+            database = os.getenv('DB_DATABASE'),
+            user = os.getenv('DB_USERNAME'),
+            password = os.getenv('DB_PASSWORD')
+        )
+        print('Database is Connected!')
+        return conection
 
-    load_dotenv()
-
-    def __init__(self):
-        self.__host = os.getenv('DB_HOST')
-        self.__port = os.getenv('DB_PORT')
-        self.__database = os.getenv('DB_DATABASE')
-        self.__user = os.getenv('DB_USERNAME')
-        self.__password = os.getenv('DB_PASSWORD')
-
-        self.__conection = None
-        self.__cursor = None
-
-        self.__conect()
-
-    def __conect(self):
-        try:
-            self.__conection = psycopg2.connect(
-                host=self.__host,
-                port=self.__port,
-                database=self.__database,
-                user=self.__user,
-                password=self.__password
-            )
-            self.__cursor = self.__conection.cursor()
-
-            print('Database is Connected!')
-
-
-        except psycopg2.Error as e:
-            print("Error while connecting to database: ", e)
-            self.__conection = None
-            self.__cursor = None
-
-
-        # finally:
-        #     self.__conection.close()
-        #     self.__cursor.close()
-
-
-    def conection(self):
-        return self.__conection
-
-    def cursor(self):
-        return self.__cursor
-
-
-conn = Conection()
+    except psycopg2.Error as e:
+        return("Error while connecting to database: ", e)
